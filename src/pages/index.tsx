bd1fd5e7094b738bf95099567f9a5901412093da
王聪 <solacowa@gmail.com>
2019-11-21 18:02:00 +0800

    change css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import styles from './index.css';
-import { Icon, Input, Button, Alert, message } from 'antd';
+import { Icon, Input, Button, Alert, message, Row, Col } from 'antd';
 import { connect } from 'dva';
 import { FormComponentProps } from 'antd/es/form';
 import { Action, Dispatch } from 'redux';
 import { StateType } from '@/pages/model';
 // @ts-ignore
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const { Search } = Input;
 
@@ -28,38 +28,40 @@ export default class Index extends Component <IndexProps> {
     return (
       <div className={styles.normal}>
         <div className={styles.wrapper}>
-          <div className={styles.body}>
-            <div className={styles.buttons}>
-              <Search
-                placeholder="请输入url地址"
-                enterButton={<span><Icon type="switch"/> 生成短链</span>}
-                size="large"
-                type="primary"
-                onSearch={(val: string) => {
-                  dispatch({
-                    type: 'index/post',
-                    payload: {
-                      url: val,
-                    },
-                  });
-                }}
-              />
-            </div>
-            {data && data.code && <div className={styles.messageSizeDemo} style={{ marginTop: 10 }}>
-              <Alert
-                message="短链"
-                description={<div>
-                  <code style={{ fontWeight: 600 }}>{data.short_uri}</code>
-                  <CopyToClipboard text={data.short_uri}  onCopy={() => message.success("复制成功!")}>
-                    <Button style={{ marginLeft: 20 }}><Icon type="copy"/> 复制</Button>
-                  </CopyToClipboard>
-                </div>}
-                type="success"
-                showIcon={true}
-              />
-            </div>}
+          <Row>
+            <Col span={12} offset={6}>
+              <div className={styles.buttons}>
+                <Search
+                  placeholder="请输入url地址"
+                  enterButton={<span><Icon type="switch"/> 生成短链</span>}
+                  size="large"
+                  type="primary"
+                  onSearch={(val: string) => {
+                    dispatch({
+                      type: 'index/post',
+                      payload: {
+                        url: val,
+                      },
+                    });
+                  }}
+                />
+              </div>
+              {data && data.code && <div className={styles.messageSizeDemo} style={{ marginTop: 10 }}>
+                <Alert
+                  message="短链"
+                  description={<div>
+                    <code style={{ fontWeight: 600 }}>{data.short_uri}</code>
+                    <CopyToClipboard text={data.short_uri} onCopy={() => message.success('复制成功!')}>
+                      <Button style={{ marginLeft: 20 }}><Icon type="copy"/> 复制</Button>
+                    </CopyToClipboard>
+                  </div>}
+                  type="success"
+                  showIcon={true}
+                />
+              </div>}</Col>
 
-          </div>
+
+          </Row>
         </div>
 
 
